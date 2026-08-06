c31f81ca7324dadde26ef8dd33f25fa8a9f45041
TIGNL <tigunitnabil@gmail.com>
2026-08-06 12:44:09 +0100

    fix: remove unsupported contentWindowInsets param from ModalBottomSheet

--- app/src/main/java/com/itn/securebrowser/ui/sheets/SheetHost.kt
+++ app/src/main/java/com/itn/securebrowser/ui/sheets/SheetHost.kt
@@ -15,11 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -129,8 +124,7 @@ fun SheetHost(
                     sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
                     modifier = Modifier.fillMaxHeight(0.9f),
                     dragHandle = { BottomSheetDefaults.DragHandle() },
-                    containerColor = MaterialTheme.colorScheme.surface,
-                    contentWindowInsets = { WindowInsets.navigationBars.add(WindowInsets.ime) }
+                    containerColor = MaterialTheme.colorScheme.surface
                 ) {
                     content(sheet, isTop, dismiss)
                 }
