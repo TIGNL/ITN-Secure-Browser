f180d2bb5adf09d20aa66e65fb5adcd0971b1ab2
TIGNL <tigunitnabil@gmail.com>
2026-08-11 19:58:58 +0100

    perf: remove appcompat and material dependencies, switch to ComponentActivity and platform themes

--- app/src/main/java/com/itn/securebrowser/BaseActivity.kt
+++ app/src/main/java/com/itn/securebrowser/BaseActivity.kt
@@ -1,14 +1,14 @@
 package com.itn.securebrowser
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 
 /**
  * All activities extend this class so the app locale is applied
  * consistently everywhere. To change the app language, update
  * LocaleHelper.APP_LOCALE and rebuild.
  */
-open class BaseActivity : AppCompatActivity() {
+open class BaseActivity : ComponentActivity() {
     override fun attachBaseContext(newBase: Context) {
         super.attachBaseContext(LocaleHelper.wrap(newBase))
     }
